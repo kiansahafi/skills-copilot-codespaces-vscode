0ed35b25c7f127f68961c4beb3e358ebdacbbfd2
kian sahafi <82085912+kiansahafi@users.noreply.github.com>
2024-12-15 09:45:10 +0000

    Copilot third commit repeat

--- comments.js
+++ comments.js
@@ -1,35 +1,14 @@
 //create web server
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const querystring = require('querystring');
-const comments = [];
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const parseUrl = url.parse(req.url);
-    const path = parseUrl.pathname;
-    const query = querystring.parse(parseUrl.query);
-    if (path === '/') {
-        fs.readFile('./index.html', (err, data) => {
-            if (err) {
-                res.end('404 not found');
-            } else {
-                res.end(data);
-            }
-        });
-    } else if (path === '/comment') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        if (query.op === 'push') {
-            comments.push(query.comment);
-            res.end(JSON.stringify({ status: 'success' }));
-        } else if (query.op === 'list') {
-            res.end(JSON.stringify(comments));
-        }
-    } else {
-        res.end('404 not found');
-    }
+//create a route
+app.get('/comments', (req, res) => {
+    //send data to client
+    res.send('comments');
 });
 
-server.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
+//listen to port
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
